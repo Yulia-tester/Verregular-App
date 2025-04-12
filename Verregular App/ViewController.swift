846579b4931ce0a6b63e36053ba3bf7a07151dd9
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-04-12 10:57:34 -0500

    First commit, added gitignore file

---
+++ Verregular App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Verregular App
+//
+//  Created by Юлия Дегтярева on 2025-04-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
