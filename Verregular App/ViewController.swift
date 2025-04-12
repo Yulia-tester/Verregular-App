65e540af4b3037b13be9ffd8094abdf18bebce07
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-04-12 11:38:50 -0500

    1. Created Verb Model 2. Added localization 3. Created IrregularVerbs Model 4. Created computed property translation 5. Added UIView Table on View Controller

--- Verregular App/ViewController.swift
+++ Verregular App/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
