fbd3705e674ddbe456a4f2bb78fc1cfaf5dc41c8
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-04-14 19:41:58 -0500

    1. Added configure method in UITableViewCell 2. Set the height for the cell using a constraint 3. Or  Set the height for the cell using a  heightForRowAt

--- Verregular App/View/VerbTableViewCell.swift
+++ Verregular App/View/VerbTableViewCell.swift
@@ -12,4 +12,11 @@ final class VerbTableViewCell: UITableViewCell {
     @IBOutlet private weak var pastSimpleLabel: UILabel!
     @IBOutlet private weak var participleLabel: UILabel!
     @IBOutlet private weak var translationLabel: UILabel!
+    
+    func configure(for verb: Verb) {
+        infinitiveLabel.text = verb.infinitive
+        pastSimpleLabel.text = verb.pastSimple
+        participleLabel.text = verb.participle
+        translationLabel.text = verb.translation
+    }
 }
